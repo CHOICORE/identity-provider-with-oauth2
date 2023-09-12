6718bad163046ac2b884ac2728ed34badec20552
CHOICORE <choijaehyeong93@gmail.com>
2023-09-12 20:39:17 +0900

    feat: add authentication properties

---
+++ api/identity-provider/src/main/kotlin/me/choicore/likeapuppy/identityprovider/common/properties/AuthenticationProperties.kt
@@ -0,0 +1,8 @@
+package me.choicore.likeapuppy.identityprovider.common.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "authentication")
+data class AuthenticationProperties(
+	val loginAttemptsLimit: Int,
+)
