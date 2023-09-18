acc2e35bcbce9ac6c742d4cea8088300704c74f3
CHOICORE <choijaehyeong93@gmail.com>
2023-09-18 16:25:07 +0900

    test: authorization service

--- api/identity-provider/src/test/kotlin/me/choicore/likeapuppy/identityprovider/AuthorizationGrantTypeTests.kt
+++ api/identity-provider/src/test/kotlin/me/choicore/likeapuppy/identityprovider/AuthorizationGrantTypeTests.kt
@@ -8,232 +8,168 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.MultiValueMap
 import org.springframework.web.util.UriComponentsBuilder
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
+@Transactional
 class AuthorizationGrantTypeTests(
-	private val mockMvc: MockMvc,
-	private var registeredClientRepository: RegisteredClientRepository,
+    private val mockMvc: MockMvc,
 ) {
 
-	companion object {
-		private const val REDIRECT_URI = "http://127.0.0.1:8080/test"
-		private const val TOKEN_ENDPOINT = "/oauth2/token"
-		private const val AUTHORIZATION_URI = "/oauth2/authorize"
+    companion object {
+        private const val REDIRECT_URI = "http://127.0.0.1:8080/login/oauth2/code/like-a-puppy-developer-client"
+        private const val TOKEN_ENDPOINT = "/oauth2/token"
+        private const val AUTHORIZATION_URI = "/oauth2/authorize"
 
-		private val AUTHORIZATION_REQUEST: String = UriComponentsBuilder
-			.fromPath(AUTHORIZATION_URI)
-			.queryParam("response_type", "code")
-			.queryParam("client_id", "like-a-puppy")
-			.queryParam("client_secret", "like-a-puppy")
-			.queryParam("scope", "openid")
-			.queryParam("state", "state")
-			.queryParam("redirect_uri", REDIRECT_URI)
-			.toUriString()
-	}
+        private val AUTHORIZATION_REQUEST: String = UriComponentsBuilder
+            .fromPath(AUTHORIZATION_URI)
+            .queryParam("response_type", "code")
+            .queryParam("client_id", "like-a-puppy")
+            .queryParam("client_secret", "like-a-puppy-secret")
+            .queryParam("scope", "openid")
+            .queryParam("state", "state")
+            .queryParam("redirect_uri", REDIRECT_URI)
+            .toUriString()
+    }
 
-	@Test
-	@DisplayName("grant_type=authorization_code 을 통해 사용자에게 인증 토큰을 발급한다.")
-	@WithMockUser(username = "1", password = "1", roles = ["USER"])
-	fun whenUserRequestAuthorizationThenGetAccessTokenWithGrantedAuthorizationCode() {
-		val mvcResult: MvcResult = mockMvc.get(AUTHORIZATION_REQUEST)
-			.andExpect {
-				status { is3xxRedirection() }
-			}.andReturn()
+    @Test
+    @DisplayName("grant_type=authorization_code 을 통해 사용자에게 인증 토큰을 발급한다.")
+    @WithMockUser(username = "1", password = "1", roles = ["USER"])
+    fun whenUserRequestAuthorizationThenGetAccessTokenWithGrantedAuthorizationCode() {
+        println(AUTHORIZATION_REQUEST)
+        val mvcResult: MvcResult = mockMvc.get(AUTHORIZATION_REQUEST)
+            .andExpect {
+                status { is3xxRedirection() }
+            }.andReturn()
 
-		val redirectedUrl: String = checkNotNull(mvcResult.response.redirectedUrl)
+        val redirectedUrl: String = checkNotNull(mvcResult.response.redirectedUrl)
 
-		assertThat(redirectedUrl).startsWith(REDIRECT_URI)
+        assertThat(redirectedUrl).startsWith(REDIRECT_URI)
 
-		val queryParams: MultiValueMap<String, String> =
-			UriComponentsBuilder.fromUriString(redirectedUrl).build().queryParams
-		val code: String = checkNotNull(queryParams.getFirst("code"))
+        val queryParams: MultiValueMap<String, String> =
+            UriComponentsBuilder.fromUriString(redirectedUrl).build().queryParams
+        val code: String = checkNotNull(queryParams.getFirst("code"))
 
-		val authorizationUri = UriComponentsBuilder
-			.fromPath(TOKEN_ENDPOINT)
-			.queryParam("grant_type", "authorization_code")
-			.queryParam("code", code)
-			.queryParam("client_id", "like-a-puppy")
-			.queryParam("client_secret", "like-a-puppy")
-			.queryParam("scope", "openid")
-			.queryParam("redirect_uri", REDIRECT_URI)
-			.toUriString()
+        val authorizationUri = UriComponentsBuilder
+            .fromPath(TOKEN_ENDPOINT)
+            .queryParam("grant_type", "authorization_code")
+            .queryParam("code", code)
+            .queryParam("client_id", "like-a-puppy")
+            .queryParam("client_secret", "like-a-puppy-secret")
+            .queryParam("scope", "openid")
+            .queryParam("redirect_uri", REDIRECT_URI)
+            .toUriString()
 
-		mockMvc.post(authorizationUri)
-			.andExpect {
-				status { isOk() }
-				content {
-					contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
-				}
-			}
-			.andDo { print() }
-	}
+        mockMvc.post(authorizationUri)
+            .andExpect {
+                status { isOk() }
+                content {
+                    contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
+                }
+            }
+            .andDo { print() }
+    }
 
-	@Test
-	@DisplayName("grant_type=client_credentials 을 통해 사용자에게 인증 토큰을 발급한다.")
-	@WithMockUser(username = "1", password = "1", roles = ["USER"])
-	fun whenUserRequestAuthorizationThenGetAccessTokenWithGrantedClientCredentials() {
-		val clientId: Pair<String, String> = Pair("client_id", "like-a-puppy")
-		val clientSecret: Pair<String, String> = Pair("client_secret", "like-a-puppy")
+    @Test
+    @DisplayName("grant_type=client_credentials 을 통해 사용자에게 인증 토큰을 발급한다.")
+    fun whenUserRequestAuthorizationThenGetAccessTokenWithGrantedClientCredentials() {
+        val clientId: Pair<String, String> = Pair("client_id", "like-a-puppy")
+        val clientSecret: Pair<String, String> = Pair("client_secret", "like-a-puppy-secret")
 
-		mockMvc.post(TOKEN_ENDPOINT) {
-			headers {
-				HttpHeaders().setBasicAuth(clientId.second, clientSecret.second)
-			}
-			param("grant_type", "client_credentials")
-			param(clientId.first, clientId.second)
-			param(clientSecret.first, clientSecret.second)
-			param("scope", "openid")
-			param("state", "state")
-		}.andExpect {
-			status { isOk() }
-			content {
-				contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
-				jsonPath("\$.access_token") { isNotEmpty() }
-				jsonPath("\$.token_type") { value("Bearer") }
-				jsonPath("\$.expires_in") { isNumber() }
-			}
-		}
-	}
+        mockMvc.post(TOKEN_ENDPOINT) {
+            headers {
+                HttpHeaders().setBasicAuth(clientId.second, clientSecret.second)
+            }
+            param("grant_type", "client_credentials")
+            param(clientId.first, clientId.second)
+            param(clientSecret.first, clientSecret.second)
+            param("scope", "openid")
+            param("state", "state")
+        }.andExpect {
+            status { isOk() }
+            content {
+                contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
+                jsonPath("\$.access_token") { isNotEmpty() }
+                jsonPath("\$.token_type") { value("Bearer") }
+                jsonPath("\$.expires_in") { isNumber() }
+            }
+        }
+    }
 
-	@Test
-	@DisplayName("OpenID Connect 을 통해 사용자 정보를 조회한다.")
-	@WithMockUser(username = "1")
-	fun whenRequestOidcAuthorizationThenGetIdTokenForOpenIdConnect() {
-		val clientId: Pair<String, String> = Pair("client_id", "like-a-puppy")
-		val clientSecret: Pair<String, String> = Pair("client_secret", "like-a-puppy")
+    @Test
+    @DisplayName("OpenID Connect 를 통해 사용자 정보를 조회한다.")
+    @WithMockUser(username = "1", password = "1", roles = ["USER"])
+    fun whenRequestOidcAuthorizationThenGetIdTokenForOpenIdConnect() {
+        val clientId: Pair<String, String> = Pair("client_id", "like-a-puppy")
+        val clientSecret: Pair<String, String> = Pair("client_secret", "like-a-puppy-secret")
 
-		// 1. Authorization Code Flow
-		val getAuthorizationCodeResult: MvcResult = mockMvc
-			.get(AUTHORIZATION_URI) {
-				param("response_type", "code")
-				param(clientId.first, clientId.second)
-				param(clientSecret.first, clientSecret.second)
-				param("scope", "openid")
-				param("state", "state")
-				param("redirect_uri", REDIRECT_URI)
-			}
-			.andExpect {
-				status { is3xxRedirection() }
-			}
-			.andReturn()
+        // 1. Authorization Code Flow
+        val getAuthorizationCodeResult: MvcResult = mockMvc
+            .get(AUTHORIZATION_URI) {
+                param("response_type", "code")
+                param(clientId.first, clientId.second)
+                param(clientSecret.first, clientSecret.second)
+                param("scope", "openid")
+                param("state", "state")
+                param("redirect_uri", REDIRECT_URI)
+            }
+            .andExpect {
+                status { is3xxRedirection() }
+            }
+            .andReturn()
 
-		val redirectedUrl: String = checkNotNull(getAuthorizationCodeResult.response.redirectedUrl)
+        val redirectedUrl: String = checkNotNull(getAuthorizationCodeResult.response.redirectedUrl)
 
-		assertThat(redirectedUrl).startsWith(REDIRECT_URI)
+        assertThat(redirectedUrl).startsWith(REDIRECT_URI)
 
-		val queryParams: MultiValueMap<String, String> =
-			UriComponentsBuilder.fromUriString(redirectedUrl).build().queryParams
-		// 2. Get Issued Token
-		val getIssuedTokenResult = mockMvc
-			.post(TOKEN_ENDPOINT) {
-				param("grant_type", "authorization_code")
-				param("code", checkNotNull(queryParams.getFirst("code")))
-				param(clientId.first, clientId.second)
-				param(clientSecret.first, clientSecret.second)
-				param("scope", "openid")
-				param("redirect_uri", REDIRECT_URI)
-			}
-			.andExpect {
-				status { isOk() }
-				content {
-					contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
-					jsonPath("\$.id_token") { isNotEmpty() }
-				}
-			}
-			.andDo { print() }
-			.andReturn()
+        val queryParams: MultiValueMap<String, String> =
+            UriComponentsBuilder.fromUriString(redirectedUrl).build().queryParams
+        // 2. Get Issued Token
+        val getIssuedTokenResult = mockMvc
+            .post(TOKEN_ENDPOINT) {
+                param("grant_type", "authorization_code")
+                param("code", checkNotNull(queryParams.getFirst("code")))
+                param(clientId.first, clientId.second)
+                param(clientSecret.first, clientSecret.second)
+                param("scope", "openid")
+                param("redirect_uri", REDIRECT_URI)
+            }
+            .andExpect {
+                status { isOk() }
+                content {
+                    contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
+                    jsonPath("\$.id_token") { isNotEmpty() }
+                }
+            }
+            .andDo { print() }
+            .andReturn()
 
-		val jsonObject: MutableMap<String, Any> = JSONObjectUtils
-			.parse(getIssuedTokenResult.response.contentAsString)
+        val jsonObject: MutableMap<String, Any> = JSONObjectUtils
+            .parse(getIssuedTokenResult.response.contentAsString)
 
-		val authorizationHeader = "${jsonObject["token_type"]} ${jsonObject["access_token"]}"
+        val authorizationHeader = "${jsonObject["token_type"]} ${jsonObject["access_token"]}"
 
-		// 3. Get User Info
-		mockMvc
-			.get("/userinfo") {
-				header(HttpHeaders.AUTHORIZATION, authorizationHeader)
-			}
-			.andExpect {
-				status { isOk() }
-				content {
-					contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
-					jsonPath("\$.sub") { value("1") }
-				}
-			}
-	}
-
-	@Test
-	@DisplayName("Access Token 을 통해 사용자 정보를 조회한다.")
-	@WithMockUser(username = "1")
-	fun whenRequestAuthorizationThenGetUserInfoWithAccessToken() {
-		val clientId: Pair<String, String> = Pair("client_id", "like-a-puppy")
-		val clientSecret: Pair<String, String> = Pair("client_secret", "like-a-puppy")
-
-		// 1. Authorization Code Flow
-		val getAuthorizationCodeResult: MvcResult = mockMvc
-			.get(AUTHORIZATION_URI) {
-				param("response_type", "code")
-				param(clientId.first, clientId.second)
-				param(clientSecret.first, clientSecret.second)
-				param("scope", "openid")
-				param("state", "state")
-				param("redirect_uri", REDIRECT_URI)
-			}
-			.andExpect {
-				status { is3xxRedirection() }
-			}
-			.andReturn()
-
-		val redirectedUrl: String = checkNotNull(getAuthorizationCodeResult.response.redirectedUrl)
-
-		assertThat(redirectedUrl).startsWith(REDIRECT_URI)
-
-		val queryParams: MultiValueMap<String, String> =
-			UriComponentsBuilder.fromUriString(redirectedUrl).build().queryParams
-
-		// 2. Get Issued Token
-		val getIssuedTokenResult =
-			mockMvc.post(TOKEN_ENDPOINT) {
-				param("grant_type", "authorization_code")
-				param("code", checkNotNull(queryParams.getFirst("code")))
-				param(clientId.first, clientId.second)
-				param(clientSecret.first, clientSecret.second)
-				param("scope", "openid")
-				param("redirect_uri", REDIRECT_URI)
-			}.andExpect {
-				status { isOk() }
-				content {
-					contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
-					jsonPath("\$.access_token") { isNotEmpty() }
-				}
-			}
-				.andDo { print() }.andReturn()
-
-		val jsonObject: MutableMap<String, Any> = JSONObjectUtils
-			.parse(getIssuedTokenResult.response.contentAsString)
-
-		val authorizationHeader = "${jsonObject["token_type"]} ${jsonObject["access_token"]}"
-
-		// 3. Get User Info
-		mockMvc.get("/userinfo") {
-			header(HttpHeaders.AUTHORIZATION, authorizationHeader)
-		}.andExpect {
-			status { isOk() }
-			content {
-				contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
-				jsonPath("\$.sub") { value("1") }
-			}
-		}
-	}
+        // 3. Get User Info
+        mockMvc
+            .get("/userinfo") {
+                header(HttpHeaders.AUTHORIZATION, authorizationHeader)
+            }
+            .andExpect {
+                status { isOk() }
+                content {
+                    contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
+                    jsonPath("\$.sub") { value("1") }
+                }
+            }
+    }
 }
--- api/identity-provider/src/test/kotlin/me/choicore/likeapuppy/identityprovider/IdentityProviderApplicationTests.kt
+++ api/identity-provider/src/test/kotlin/me/choicore/likeapuppy/identityprovider/IdentityProviderApplicationTests.kt
@@ -4,7 +4,6 @@ import com.gargoylesoftware.htmlunit.Page
 import com.gargoylesoftware.htmlunit.WebClient
 import com.gargoylesoftware.htmlunit.WebResponse
 import com.gargoylesoftware.htmlunit.html.HtmlButton
-import com.gargoylesoftware.htmlunit.html.HtmlElement
 import com.gargoylesoftware.htmlunit.html.HtmlInput
 import com.gargoylesoftware.htmlunit.html.HtmlPage
 import org.assertj.core.api.Assertions
@@ -22,105 +21,71 @@ import java.io.IOException
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @AutoConfigureMockMvc
 class IdentityProviderApplicationTests(
-	val webClient: WebClient,
+    val webClient: WebClient,
 ) {
 
-	companion object {
+    companion object {
+        private const val REDIRECT_URI = "http://127.0.0.1:8080/login/oauth2/code/like-a-puppy-developer-client"
+        private const val AUTHORIZATION_URI = "/oauth2/authorize"
+        private val AUTHORIZATION_REQUEST: String = UriComponentsBuilder
+            .fromPath(AUTHORIZATION_URI)
+            .queryParam("response_type", "code")
+            .queryParam("client_id", "like-a-puppy")
+            .queryParam("client_secret", "like-a-puppy")
+            .queryParam("scope", "openid")
+            .queryParam("state", "state")
+            .queryParam("redirect_uri", REDIRECT_URI)
+            .toUriString()
 
-		private const val REDIRECT_URI = "http://127.0.0.1:8080/oauth2/authorize"
+        @Throws(IOException::class)
+        private fun <P : Page> login(
+            page: HtmlPage,
+            username: String,
+            password: String,
+        ): P {
+            val usernameInput: HtmlInput = page.querySelector("input[name=\"username\"]")
+            val passwordInput: HtmlInput = page.querySelector("input[name=\"password\"]")
+            val loginButton: HtmlButton = page.querySelector("button")
+            usernameInput.type(username)
+            passwordInput.type(password)
+            return loginButton.click()
+        }
 
-		private val AUTHORIZATION_REQUEST = UriComponentsBuilder
-			.fromPath("/oauth2/authorize")
-			.queryParam("response_type", "code")
-			.queryParam("client_id", "like-a-puppy")
-			.queryParam("client_secret", "like-a-puppy")
-			.queryParam("scope", "USER")
-			.queryParam("state", "some-state")
-			.queryParam("redirect_uri", REDIRECT_URI)
-			.toUriString()
+        private fun assertLoginPage(page: HtmlPage) {
+            Assertions.assertThat(page.getUrl().toString()).endsWith("/login")
+            val usernameInput: HtmlInput = page.querySelector("input[name=\"username\"]")
+            val passwordInput: HtmlInput = page.querySelector("input[name=\"password\"]")
+            val loginButton: HtmlButton = page.querySelector("button")
+            Assertions.assertThat(usernameInput).isNotNull()
+            Assertions.assertThat(passwordInput).isNotNull()
+            Assertions.assertThat(loginButton.textContent).isEqualTo("로그인")
+        }
+    }
 
-		@Throws(IOException::class)
-		private fun <P : Page> login(
-			page: HtmlPage,
-			username: String,
-			password: String,
-		): P {
-			val usernameInput: HtmlInput = page.querySelector("input[name=\"username\"]")
-			val passwordInput: HtmlInput = page.querySelector("input[name=\"password\"]")
-			val loginButton: HtmlButton = page.querySelector("button")
-			usernameInput.type(username)
-			passwordInput.type(password)
-			return loginButton.click()
-		}
+    @BeforeEach
+    fun setUp() {
+        webClient.options.isThrowExceptionOnFailingStatusCode = false
+        webClient.options.isRedirectEnabled = true
+        webClient.cookieManager.clearCookies()
+    }
 
-		private fun assertLoginPage(page: HtmlPage) {
-			Assertions.assertThat(page.getUrl().toString()).endsWith("/login")
-			val usernameInput: HtmlInput = page.querySelector("input[name=\"username\"]")
-			val passwordInput: HtmlInput = page.querySelector("input[name=\"password\"]")
-			val loginButton: HtmlButton = page.querySelector("button")
-			Assertions.assertThat(usernameInput).isNotNull()
-			Assertions.assertThat(passwordInput).isNotNull()
-			Assertions.assertThat(loginButton.textContent).isEqualTo("로그인")
-		}
-	}
+    @Test
+    @DisplayName("로그인 성공 시 200 코드 반환")
+    @Throws(IOException::class)
+    fun whenLoginSuccessfulThenHttpStatusOK() {
+        val htmlPage: HtmlPage = webClient.getPage("/")
 
-	@BeforeEach
-	fun setUp() {
-		println(AUTHORIZATION_REQUEST)
-		webClient.options.isThrowExceptionOnFailingStatusCode = true
-		webClient.options.isRedirectEnabled = true
-		webClient.cookieManager.clearCookies()
-	}
+        assertLoginPage(htmlPage)
 
-	@Test
-	@DisplayName("로그인 성공 시 200 코드 반환")
-	@Throws(IOException::class)
-	fun whenLoginSuccessfulThenHttpStatusOK() {
-		val htmlPage: HtmlPage = webClient.getPage("/")
+        val webResponse: WebResponse = login<HtmlPage>(htmlPage, "1", "1").webResponse
 
-		assertLoginPage(htmlPage)
+        Assertions.assertThat(webResponse.statusCode).isEqualTo(HttpStatus.OK.value())
+    }
 
-		val webResponse: WebResponse = login<HtmlPage>(htmlPage, "1", "1").webResponse
-
-		Assertions.assertThat(webResponse.statusCode).isEqualTo(HttpStatus.OK.value())
-	}
-
-// 	@Test
-// 	@Throws(IOException::class)
-// 	fun whenLoggingInAndRequestingTokenThenRedirectsToClientApplication() {
-// 		// Log in
-// 		webClient.options.isThrowExceptionOnFailingStatusCode = false
-// 		webClient.options.isRedirectEnabled = false
-// 		login<Page>(webClient.getPage("/login"), "1", "1")
-//
-// 		// Request token
-// 		val webResponse: WebResponse = webClient.getPage<Page>(AUTHORIZATION_REQUEST).webResponse
-// 		Assertions.assertThat(webResponse.statusCode).isEqualTo(HttpStatus.MOVED_PERMANENTLY.value())
-// 		val location: String = webResponse.getResponseHeaderValue("location")
-// 		println("Response Header - Location > $location")
-// 		Assertions.assertThat(location).startsWith(REDIRECT_URI)
-// 		Assertions.assertThat(location).contains("code=")
-// 	}
-
-	@Test
-	@DisplayName("로그인 되지 않은 상태로 인증 요청 시 로그인 페이지로 리다이렉트")
-	@Throws(IOException::class)
-	fun whenNotLoggedInAndRequestingTokenThenRedirectsToLogin() {
-		assertLoginPage(webClient.getPage(AUTHORIZATION_REQUEST))
-	}
-
-	@Test
-	@DisplayName("로그인 실패 시 에러 메세지 출력")
-	@Throws(IOException::class)
-	fun whenLoginFailsThenDisplayBadCredentials() {
-		val htmlPage: HtmlPage = webClient.getPage("/")
-
-		val loginErrorPage: HtmlPage = login(htmlPage, "2", "2")
-
-		val alert: HtmlElement = loginErrorPage.querySelector("div[role=\"alert\"]")
-
-		Assertions.assertThat(alert).isNotNull()
-
-		Assertions.assertThat(alert.asNormalizedText()).isEqualTo("Invalid username or password.")
-	}
+    @Test
+    @DisplayName("로그인 되지 않은 상태로 인증 요청 시 로그인 페이지로 리다이렉트")
+    @Throws(IOException::class)
+    fun whenNotLoggedInAndRequestingTokenThenRedirectsToLogin() {
+        assertLoginPage(webClient.getPage(AUTHORIZATION_REQUEST))
+    }
 }
