ec85edf175a7f5698ee701a97e8478a4dfbc78e4
CHOICORE <choijaehyeong93@gmail.com>
2023-09-19 18:00:52 +0900

    feat: add logging

--- api/identity-provider/src/main/kotlin/me/choicore/likeapuppy/identityprovider/web/view/LoginController.kt
+++ api/identity-provider/src/main/kotlin/me/choicore/likeapuppy/identityprovider/web/view/LoginController.kt
@@ -1,12 +1,13 @@
 package me.choicore.likeapuppy.identityprovider.web.view
 
 import jakarta.servlet.http.HttpServletRequest
+import me.choicore.likeapuppy.identityprovider.common.Slf4j
 import me.choicore.likeapuppy.identityprovider.exception.UNAUTHORIZED_ERROR
 import me.choicore.likeapuppy.identityprovider.exception.getUnauthorizedError
+import org.slf4j.Logger
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.WebAttributes
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
@@ -14,30 +15,34 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes
 @Controller
 class LoginController {
 
+    private val logger: Logger = Slf4j
+
+    @GetMapping
+    fun home(): String = "index"
+
     @GetMapping("/login")
-    fun goingToLoginPage(
-        request: HttpServletRequest,
-        model: Model,
-    ): String {
-        return "login"
-    }
+    fun goingToLoginPage(): String = "login"
 
     @PostMapping("/login-error")
     fun redirectToLoginPageWithErrorMessage(
         request: HttpServletRequest,
         redirectAttributes: RedirectAttributes,
     ): String {
-        request.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION)?.let {
-            redirectAttributes.addFlashAttribute(
-                UNAUTHORIZED_ERROR,
-                getUnauthorizedError(it as AuthenticationException),
-            )
+        logger.info("Login attempt failed. Redirecting to the login page with an accompanying error message.")
+        request.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION)?.let { authenticationException: Any ->
+            when (authenticationException) {
+                is AuthenticationException -> {
+                    redirectAttributes.addFlashAttribute(
+                        UNAUTHORIZED_ERROR,
+                        getUnauthorizedError(authenticationException),
+                    )
+                }
+
+                else -> {
+                    logger.error("Unknown attributes: $authenticationException")
+                }
+            }
         }
         return "redirect:/login"
-    }
-
-    @GetMapping
-    fun home(): String {
-        return "index"
     }
 }
