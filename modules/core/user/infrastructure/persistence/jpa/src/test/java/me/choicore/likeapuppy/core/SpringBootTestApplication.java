60af2e5d13601f3c43fe73ae87adbea20a4eaefd
CHOICORE <choijaehyeong93@gmail.com>
2023-10-09 15:14:15 +0900

    fix: remove excludeFilters

--- modules/core/user/infrastructure/persistence/jpa/src/test/java/me/choicore/likeapuppy/core/SpringBootTestApplication.java
+++ modules/core/user/infrastructure/persistence/jpa/src/test/java/me/choicore/likeapuppy/core/SpringBootTestApplication.java
@@ -1,18 +1,10 @@
 package me.choicore.likeapuppy.core;
 
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-
-import static org.springframework.context.annotation.ComponentScan.Filter;
 
 
 @EnableAutoConfiguration
-@ComponentScan(excludeFilters = {
-        @Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
-        @Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class)
-})
+@ComponentScan
 public class SpringBootTestApplication {
 }
