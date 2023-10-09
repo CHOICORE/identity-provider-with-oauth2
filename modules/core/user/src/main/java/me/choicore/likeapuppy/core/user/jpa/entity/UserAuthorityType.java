0422d2fb75e763b12b098eb3ce4ff5027ab1cab3
CHOICORE <choijaehyeong93@gmail.com>
2023-10-09 16:14:27 +0900

    chore: remove files due to structure change

--- modules/core/user/src/main/java/me/choicore/likeapuppy/core/user/jpa/entity/UserAuthorityType.java
+++
@@ -1,6 +0,0 @@
-package me.choicore.likeapuppy.core.user.jpa.entity;
-
-public enum UserAuthorityType {
-    USER,
-    ADMIN
-}
--- modules/core/user/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO AUTHORITY( NAME
-                     , SCOPE
-                     , DESCRIPTION)
-VALUES ('USER', 'ACCOUNT', '사용자 권한'),
-       ('ADMIN', 'ACCOUNT', '관리자 권한');
